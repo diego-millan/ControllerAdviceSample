4a5c2258511c6897f76234266a491ce4e5d8251c
diego-millan <die9o.r@gmail.com>
2021-09-16 14:16:10 -0300

    Created Aspect

--- src/main/java/com/example/controlleradvice/ControllerAdviceApplication.java
+++ src/main/java/com/example/controlleradvice/ControllerAdviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ControlleradviceApplication {
+public class ControllerAdviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControlleradviceApplication.class, args);
+		SpringApplication.run(ControllerAdviceApplication.class, args);
 	}
 
 }
--- src/main/java/com/example/controlleradvice/controller/MyController.java
+++ src/main/java/com/example/controlleradvice/controller/MyController.java
@@ -4,8 +4,12 @@ import com.example.controlleradvice.model.City;
 import com.example.controlleradvice.service.ICityService;
 import java.util.List;
 import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,25 +19,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-    @Autowired
-    private ICityService cityService;
+  Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @GetMapping(value = "/cities/{id}")
-    public City getCity(@PathVariable Long id) {
+  @Autowired private ICityService cityService;
 
-        return cityService.findById(id);
+  @GetMapping(value = "/cities/{id}")
+  public ResponseEntity<City> getCity(@PathVariable Long id) {
+
+    ResponseEntity<City> response;
+
+    try {
+      City result = cityService.findById(id);
+      response = ResponseEntity.ok(result);
+
+    } catch (Exception e) {
+      response = ResponseEntity.badRequest().build();
     }
 
-    @PostMapping(value = "/cities", consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public City createCity(@RequestBody @Valid City city) {
+    return response;
+  }
 
-        return cityService.save(city);
-    }
+  @PostMapping(
+      value = "/cities",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<City> createCity(@RequestBody @Valid City city) {
 
-    @GetMapping(value = "/cities")
-    public List<City> findAll() {
+    City result = cityService.save(city);
 
-        return cityService.findAll();
-    }
+    return ResponseEntity.status(HttpStatus.CREATED).body(result);
+  }
+
+  @GetMapping(value = "/cities")
+  public ResponseEntity<List<City>> findAll() {
+
+    List<City> cities = cityService.findAll();
+    return ResponseEntity.ok(cities);
+  }
 }
